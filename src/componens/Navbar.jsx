eef7ed5f76d8311144505782d8e99c35be3d9c61
jayrboy <j.jayox_@hotmail.com>
2024-02-17 14:21:49 +0700

    Test Icons

--- src/componens/Navbar.jsx
+++ src/componens/Navbar.jsx
@@ -1,4 +1,4 @@
-import { CartIcon } from '../icons'
+import { CartIcon, Testing } from '../icons'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
@@ -9,6 +9,7 @@ const Navbar = () => {
       <div className="nav-center">
         <h3>redux toolkit</h3>
         <div className="nav-container">
+          <Testing />
           <CartIcon />
           <div className="amount-container">
             <p className="total-amount">{amount}</p>
