8a144b8fd80a8441a14801d0e9916a467ecd9121
jayrboy <j.jayox_@hotmail.com>
2024-02-17 15:25:35 +0700

    Add Reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import Navbar from './componens/Navbar'
 import CartContainer from './componens/CartContainer'
+import { useSelector, useDispatch } from 'react-redux'
+import { calculateTotals } from './redux/cart/cartSlice'
+import { useEffect } from 'react'
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
+
   return (
     <main>
       <Navbar />
