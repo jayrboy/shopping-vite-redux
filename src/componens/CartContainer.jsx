897bd8c406555145d75fb1154fab00075810df45
jayrboy <j.jayox_@hotmail.com>
2024-02-17 15:12:28 +0700

    Add Reducer Clear Cart

--- src/componens/CartContainer.jsx
+++ src/componens/CartContainer.jsx
@@ -1,8 +1,10 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import CartItem from './CartItem'
+import { clearCart } from '../redux/cart/cartSlice'
 
 const CartContainer = () => {
   const { cartItems, total, amount } = useSelector((store) => store.cart)
+  const dispatch = useDispatch()
 
   if (amount < 1) {
     return (
@@ -35,7 +37,14 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button
+          className="btn clear-btn"
+          onClick={() => {
+            dispatch(clearCart())
+          }}
+        >
+          clear cart
+        </button>
       </footer>
     </section>
   )
